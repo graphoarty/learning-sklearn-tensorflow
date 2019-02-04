483297dcbef68a3bf79897d82979f3ccd18ab04d
Quinston Pimenta <quinston@quarkexp.com>
2019-02-04 11:37:18 +0530

    initial commit

---
+++ root.py
@@ -0,0 +1,70 @@
+from sklearn.model_selection import StratifiedShuffleSplit
+import os
+import tarfile
+from six.moves import urllib
+import pandas as pd
+import numpy as np
+import hashlib
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+        tgz_path = os.path.join(housing_path, "housing.tgz")
+        urllib.request.urlretrieve(housing_url, tgz_path)
+        housing_tgz = tarfile.open(tgz_path)
+        housing_tgz.extractall(path=housing_path)
+        housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, 'housing.csv')
+    return pd.read_csv(csv_path)
+
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return [data.iloc[train_indices], data.iloc[test_indices]]
+
+
+def test_set_check(identifier, test_ratio, hash):
+    return int(hash(np.int64(identifier)).hexdigest()[-1], 16) < 16 * test_ratio
+
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+housing = load_housing_data(HOUSING_PATH)
+# housing_with_id = housing.reset_index()
+# train_data, test_data = split_train_test_by_id(housing_with_id, 0.2, 'index')
+# print(train_data)
+# print(test_data)
+
+# from sklearn.model_selection import train_test_split
+# train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+# print(test_set)
+
+housing['income_cat'] = np.ceil(housing['median_income'] / 1.5)
+housing['income_cat'].where(housing['income_cat'] < 5, 5.0, inplace=True)
+
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+for set in (strat_train_set, strat_test_set):
+    set.drop(["income_cat"], axis=1, inplace=True)
+
+housing = strat_train_set.copy()
+
